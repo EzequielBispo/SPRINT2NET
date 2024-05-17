1824d788a1f0c9d59e2c6992c774dc0c9099b250
Ezequiel Bispo <ezequielbispo011@gmail.com>
2024-05-16 22:32:13 -0300

    Arquivo Program.cs atualizado.

--- SPRINT2NET/Models/Usuario.cs
+++ SPRINT2NET/Models/Usuario.cs
@@ -1,19 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace SPRINT2NET.Models
-{ 
 {
-    [Key]
-    public int Id { get; set; }
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
 
-    [Required]
-    [StringLength(100)]
-    public string Nome { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
 
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
 
-}
-
+    }
 }
--- SPRINT2NET/Program.cs
+++ SPRINT2NET/Program.cs
@@ -1,35 +1,33 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using SPRINT2NET.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// Adicionar serviços ao contêiner.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configurar Swagger, se necessário
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Configurar o pipeline de solicitações HTTP.
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-app.UseStaticFiles();
 
-app.UseRouting();
-
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
-
-public void ConfigureServices(IServiceCollection services)
-{
-    services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-    services.AddControllersWithViews();
-}
-
